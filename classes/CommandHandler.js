74d14acabace41e3a5b1ffe5f11d44500ea4f3df
pmh-only <pmhstudio.pmh@gmail.com>
2020-03-22 11:34:36 +0900

    Standard.js & File Error Handler

--- classes/CommandHandler.js
+++ classes/CommandHandler.js
@@ -1,20 +1,21 @@
-const { readdir } = require('fs')
+const { existsSync, readdir } = require('fs')
 
 class CommandHandler {
   constructor (path) {
     this._commands = new Map()
+    if (!existsSync(path)) return
 
-    readdir(path, (err, files) => {
+    readdir(path, (err, commandFiles) => {
       if (err) console.log(err)
       else {
-        files.forEach((f) => {
-          if (!f.endsWith('.js')) return
+        commandFiles.forEach((CommandFile) => {
+          if (!CommandFile.endsWith('.js')) return
 
-          //f = f.replace('.js', '')
-          f = require(path + f)
-          let c = new f()
+          // CommandFile = CommandFile.replace('.js', '')
+          CommandFile = require(path + CommandFile)
+          const c = new CommandFile()
           this._commands.set(c.name, c)
-          if (c.aliases.length > 0) c.aliases.forEach((alias) => {this._commands.set(alias, c)})
+          if (c.aliases.length > 0) c.aliases.forEach((alias) => { this._commands.set(alias, c) })
         })
       }
     })
@@ -26,7 +27,7 @@ class CommandHandler {
 
   run (command, seoa, msg, args) {
     // Perms Check Logic here
-    
+
     // Run
     command.run(seoa, msg, args)
   }
