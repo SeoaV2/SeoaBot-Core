00bb269f3d1def19fec0cbdebbfc7d56861c2ea5
pmh-only <pmhstudio.pmh@gmail.com>
2020-03-22 20:52:10 +0900

    Error Simplifier

--- classes/ErrorHandler.js
+++ classes/ErrorHandler.js
@@ -1,12 +1,15 @@
-const os = require('os')
 const path = require('path').resolve()
+const { hostname } = require('os')
 const { WebhookClient } = require('discord.js')
+
+const djsPath = '/node_modules/discord.js/src/'
+const wsPath = '/node_modules/ws/lib/'
 
 class ErrorHandler extends Error {
   constructor (seoa, msg) {
     super(msg)
-    this.stack = '```' + this.stack.split(path).join('') + '```'
-    this.title = 'Seoa v2 Beta Error Report: from ' + os.hostname
+    this.stack = '```' + this.stack.split(path).join('seoa:/').split(djsPath).join('djs:/').split(wsPath).join('ws:/') + '```'
+    this.title = 'Seoa v2 Beta Error Report: from ' + hostname
 
     if (!seoa.webhookToken) return
     this.webhook = new WebhookClient('691241324111724574', seoa.webhookToken)
