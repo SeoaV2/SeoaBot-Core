92afd160eb08b30b308c1de27ed9681f01550322
comjun04 <comjun04@gmail.com>
2020-03-26 20:58:19 +0900

    MODERATE FIX

--- classes/LocaleHandler.js
+++ classes/LocaleHandler.js
@@ -2,7 +2,7 @@ const i18n = require('i18n')
 const path = require('path').resolve()
 
 class LocaleHandler {
-  constructor(seoa) {
+  constructor (seoa) {
     i18n.configure({
       directory: path + '/locale',
       defaultLocale: 'en_US',
@@ -14,15 +14,15 @@ class LocaleHandler {
 
     this.seoa = seoa
     this.i18n = i18n
-    this.t = (phrase, locale, ...args) => {return i18n.__({phrase, locale}, ...args)}
+    this.t = (phrase, locale, ...args) => { return i18n.__({ phrase, locale }, ...args) }
   }
 
   async getGuildLocale (id) {
     let dbData
-    try{
+    try {
       dbData = await this.seoa.knex('guild').select('locale').where('id', id)
-    } catch (err) {console.error(err.stack); return 'en_US'}
-    if(dbData.length < 1) return 'en_US'
+    } catch (err) { console.error(err.stack); return 'en_US' }
+    if (dbData.length < 1) return 'en_US'
     else return dbData[0].locale
   }
 }
